29d18e902723a26fd89815657616b1060f6ca1b8
pvantol99 <63127207+pvantol99@users.noreply.github.com>
2024-08-14 14:01:14 -0700

    Initiate Stat Scraper

---
+++ Swiss-soccer-data-scraper/player_scraper.py
@@ -0,0 +1,9 @@
+import time as t
+import hashlib
+import io
+import logging
+import os
+#import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from datetime import datetime
