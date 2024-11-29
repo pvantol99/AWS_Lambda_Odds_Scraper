6553f6c3ca3b20e3e5d0aa50993faa36de346cb6
pvantol99 <pieterkfv.vantol@gmail.com>
2024-11-29 18:56:50 -0500

    empty_list_fix
    (hopefully) final empty list fix

--- pinnacle-scraper-lambda-container-master/app/scraper/scraper.py
+++ pinnacle-scraper-lambda-container-master/app/scraper/scraper.py
@@ -13,6 +13,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.chrome.options import Options 
 from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
 from datetime import datetime
 import random 
 
@@ -32,6 +33,32 @@ class OddsScraper:
         self.service = Service(executable_path='/usr/bin/chromedriver')
         self.driver = webdriver.Chrome(service = self.service, options=self.__get_default_chrome_options())
 
+    def class_finder(self,content):
+        soup = BeautifulSoup(content , 'html.parser') 
+        classes = []
+        for i in soup.find_all( 'div' ):
+            if i.has_attr( "class" ):
+                if len(i['class']) > 0:
+                    if ('container' in i['class'][0]) & (i['class'][0] not in classes):
+                        classes.append(i['class'][0])
+        return classes
+
+    def find_odds(self,classes):
+        for i in range(0,len(classes)):
+            content = self.driver.find_elements(By.CLASS_NAME,classes[i])
+            for j in range(0,len(content)):
+                if all(elem in content[j].text.split('\n') for elem in ['1','X','2','HANDICAP','OVER','UNDER']):
+                    return content[j].text.split('\n')
+        return print ('No Odds found')
+
+
+    def find_odds(self,classes):
+        for i in range(0,len(classes)):
+            content = self.driver.find_elements(By.CLASS_NAME,classes[i])
+            for j in range(0,len(content)):
+                if all(elem in content[j].text.split('\n') for elem in ['1','X','2','HANDICAP','OVER','UNDER']):
+                    return content[j].text.split('\n')
+        return print ('No Odds found')
 
     def get_odds(self,sport1 = 'Basketball', country1 = 'USA',league1 = 'NBA'):
         #search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
@@ -39,7 +66,7 @@ class OddsScraper:
         print(urlnew)
         self.driver.get(urlnew)
         ### Time to load
-        t.sleep(7)
+        t.sleep(3)
         p_links = self.driver.find_elements(By.TAG_NAME,'p')
         for p in range(0,len(p_links)):
                 #print(p_links[p].text)
@@ -49,19 +76,17 @@ class OddsScraper:
 
         ### More loading time
         t.sleep(3)
+
         ### Get Raw Info
-        content = self.driver.find_elements(By.CLASS_NAME,'container-H9Oy8TIpsb')
-        print(content)
-        #c_refined = content[0].text.split('\n') 
-        c_refined = []
-        for c in range(0,len(content)):
-                if content[c].text.split('\n')[0] == 'Home':
-                    c_refined.append(content[c].text.split('\n'))
-        #        print(c_refined)
+        classes = self.class_finder(self.driver.page_source)
+        c_refined = self.find_odds(classes)
+        print(c_refined)
+
+        #Cut off parts of result we don't want
         for i in range(0,len(c_refined)):
-            if len(c_refined[i]) > 21:
-                end = max([i for i,x in enumerate(c_refined[i]) if '+' in x])
-        c_refined2 = c_refined[i][:end]
+            if len(c_refined) > 21:
+                end = max([i for i,x in enumerate(c_refined) if '+' in x])
+        c_refined2 = c_refined[:end]
         return c_refined2
 
     def close_connection(self):
